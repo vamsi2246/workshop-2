e90290703dc7b9c9725ad663085cbfde8912b056
Vamsi <kummaravamsi2246@gmail.com>
2026-02-25 16:23:09 +0530

    added cli file

--- cli.ts
+++ cli.ts
@@ -1,5 +1,11 @@
 const {command} = require("commander")
 
+const program = new command()
 
+program
+.command("greet <name>")
+.action((name) => {
+  console.log(`Hello, ${name}!`);
+});
 
-
+program.parse();
